79e147e333d9521f43b061ce6f1a1586340300ca
jdelledonne <j.delledonne4@gmail.com>
2021-09-28 14:30:31 -0400

    sort array by parity II - init

---
+++ sortArrayByParityII.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Title:    sortArrayByParityII.py
+# Author:   Joe Delle Donne
+# Date:     09/28/2021
+
+from typing import List
+
+class Solution:
+    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+        print("hello")
+
+if __name__ == "__main__":
+    solution = Solution
+    solution.sortArrayByParityII(solution, [])
